cb8472ae024e4d79e6f837edd05b0ad3eb99e935
AyaSKhalil <ayasamir12493@gmail.com>
2020-07-08 16:18:56 -0700

    Submission for Module1

---
+++ IN/AyaKhalil/Module1/Cont.py
@@ -0,0 +1,14 @@
+print("For-Cont")
+for letter in 'Python':
+    if letter =='h':
+        continue
+    print('Current Letter :', letter)
+
+print("\nWile-Continue")
+var = 10
+while var > 0 :
+    var = var-1
+    if var==5:
+        continue
+    print ('Current variable value:', var)
+print("Good bye")
