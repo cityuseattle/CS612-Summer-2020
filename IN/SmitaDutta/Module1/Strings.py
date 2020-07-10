57db57b4e0a22631224ed1c744ba715d998ea9c0
DuttaSmita <duttasmita@cityuniversity.edu>
2020-07-09 20:08:09 -0700

    Submission for Module 1

---
+++ IN/SmitaDutta/Module1/Strings.py
@@ -0,0 +1,12 @@
+message = "this is also a string"
+
+print("Tite:" + message.title())
+
+print("Uppercase:" +message.upper())
+
+print("Lowercase:" +message.lower())
+
+first_message = "Hi !"
+second_message = " How are you ?"
+full_message = f"{first_message} {second_message}"
+print(full_message)
