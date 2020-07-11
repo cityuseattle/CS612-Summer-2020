e4dd489ced93f56ab1ae4765c1d6badfbd54fd8e
Luis Linares Medina <linarmed@gmail.com>
2020-07-11 10:32:05 -0700

    Fibonacci PE01

--- ON/linaresmedinaluis/PE01/PE01-fibonacci.py
+++ ON/linaresmedinaluis/PE01/PE01-fibonacci.py
@@ -22,22 +22,25 @@ def GetFibonacciSequence(SequenceNumber):
         
         #Add the new sum to the fibonacci list
         CacheFibonacci.append(CurrentFibonacci) 
-        
+
+        print(f'{CurrentFibonacci}\n') 
         #Return the fibonacci list
         return CurrentFibonacci 
   
 #Main module
-try:
-    Value = int(input("Please enter a number to get fibonacci sequence:\n"))
     
-    #List for fibonacci numbers with initial numbers 0 and 1
-    CacheFibonacci = [0,1]   
-    
-    #Call function
-    Sequence = GetFibonacciSequence(Value)
-    
-    #Display sequence
-    print(f'The fibonacci sequence for {Value} is {Sequence}') 
-    
-except ValueError:
-  print (f'That was not a number, Please run it again')
+#Default value
+Value = 100
+
+#List for fibonacci numbers with initial numbers 0 and 1
+CacheFibonacci = [0,1]   
+
+print(f'The fibonacci sequence :\n0\n\n1\n ')
+
+#Call function
+Sequence = GetFibonacciSequence(Value)
+
+#Display sequence
+print(f'The fibonacci sequence for {Value} is {Sequence}\n\n') 
+print(f'Good bye!\n\n') 
+
