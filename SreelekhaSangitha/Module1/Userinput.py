29aa586405c85ec9a639f16d0c0d297f75a8d2ac
Sreelekha Sangitha <sangithasreelekha@cityuniversity.edu>
2020-07-12 11:45:19 -0700

    Submission for Module 1

---
+++ SreelekhaSangitha/Module1/Userinput.py
@@ -0,0 +1,6 @@
+name=input("Please enter your name:")
+print("\nWlcome to python,"+name+"!")
+print("the type of the variable name is" ,type(name))
+age=input("\nEnter your age:")
+print("\nYour age is" +age)
+print("The type of the variable is" ,type(age))
