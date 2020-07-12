1b716c674f1dd3fa342d58557f60912503f675de
aviatortj <ktj81m@hotmail.com>
2020-07-12 02:08:45 -0700

    Submission for Module1

---
+++ IN/TaejinKim/Module1/variable.py
@@ -0,0 +1,18 @@
+print("\nMultiple assignment\n")
+
+a, b, c =5, 3.2, "Hello"
+print ("a = ",a)
+print ("b = ",b)
+print ("c = ",c)
+
+x = y= z = "Python"
+print ("x=" + x)
+print ("y=" + y)
+print ("z=" + z)
+
+i = 5
+z = 2+ 3*i
+
+print(z)
+
+print (z>9)
