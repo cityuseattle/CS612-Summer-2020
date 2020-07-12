7a2e8903aaa8899ab43f71f1bd9aa535fbadf361
Derek Bugg <buggderek@cityuniversity.edu>
2020-07-12 13:42:46 -0700

    Submission for Module 1

---
+++ ON/DerekBugg/Module1/Strings.py
@@ -0,0 +1,8 @@
+message = "this is also a string"
+print("Uppercase :" +message.upper())
+print("Lowercase :" +message.lower())
+
+first_message = "Hi !"
+second_message = "How are you ?"
+full_message = f"{first_message} {second_message}"
+print(full_message)
