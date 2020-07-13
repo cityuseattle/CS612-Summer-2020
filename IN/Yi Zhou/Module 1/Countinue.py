2c7c95d5c67f311a0087f99750ed160b567cb739
zhui <392513460@qq.com>
2020-07-12 18:20:09 -0700

    Submission module 1

---
+++ IN/Yi Zhou/Module 1/Countinue.py
@@ -0,0 +1,15 @@
+print("For-Contine")
+for letter in 'Python':
+    if letter=='h':
+        continue
+print('Current Letter :',letter)
+
+print("\nWhile-Continue")
+var=10
+while var>0:
+    var=var-1
+    if var==5:
+        continue
+    print('Current varible value :',var)
+
+print("Good bye!")
---
+++ IN/Yi Zhou/Module 1/varible.py
@@ -0,0 +1,10 @@
+print("\nMultiple assignments\n")
+a,b,c=5,3.2,"hello"
+print("a=",a)
+print("b=",b)
+print("c=",c)
+
+x=y=z="Python"
+print("X="+x)
+print("y="+y)
+print("z="+z)
