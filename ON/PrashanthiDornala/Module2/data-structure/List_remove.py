76f7a75d1a5050f800ae79e5ebc8b092e3022fba
dornalaPrashanthi <dornalaPrashanthi12@gmail.com>
2020-07-18 17:37:23 +0530

    Submission for Module 2

---
+++ ON/PrashanthiDornala/Module2/data-structure/List_remove.py
@@ -0,0 +1,8 @@
+motorcycles=['honda','yamaha','suzuki','ducati']
+motorcycles.remove('ducati')
+#popped_motorcycle=motorcycles.pop()
+#del motorcycles[1]
+print(motorcycles)
+#print(popped_motorcycle)
+#first_owned=motorcycles.pop(0)
+#print("The first motorcycle I owned was a",first_owned)
