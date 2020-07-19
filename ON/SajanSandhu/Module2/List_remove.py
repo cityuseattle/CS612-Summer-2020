31e5b8c32ea061daea85bc1f385a334923332cc1
sajan sandhu <sajan.s.sandhu@gmail.com>
2020-07-18 23:27:31 -0700

    Submission for Module 2

---
+++ ON/SajanSandhu/Module2/List_remove.py
@@ -0,0 +1,15 @@
+#del
+motorcycles =  ['honda', 'yamaha', 'suzuki']
+del motorcycles[1]
+print(motorcycles)
+#pop
+motorcycles =  ['honda', 'yamaha', 'suzuki']
+popped_motocycles = motorcycles.pop()
+print(motorcycles)
+print(popped_motocycles)
+first_owned = motorcycles.pop(0)
+print("The first motorcycle I owned was a ", first_owned)
+#remove
+motorcycles =  ['honda', 'yamaha', 'suzuki', 'ducati']
+motorcycles.remove('ducati')
+print(motorcycles)
---
+++ ON/SajanSandhu/Module2/tuple.py
@@ -0,0 +1,2 @@
+courses = ('CS101', 2.0, 0)
+courses[1] = 4.0
