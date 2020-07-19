59cc3675038d4ff9436fd3fe76e415de6e2d994f
Yesuf <46637371+yuyeabdissa@users.noreply.github.com>
2020-07-19 10:54:02 -0700

    Submission for Module 2

---
+++ ON/YesufAbdissa/Module2/data-structure/count_character.py
@@ -0,0 +1,14 @@
+import pprint
+
+message = """Peter Piper picked a peck of picked peppers
+             A peck pickled peppers peter Piper picked
+             If peter Piper picked a peck of pickled peppers
+             Where's the peck of pickled peppers Peter Piper picked?"""
+
+
+count = {}
+for character in message:
+    count.setdefault(character, 0)
+    count[character] = count[character] + 1
+
+pprint.pprint(count)
