e5c217beba06a74f04397895edd6fdf74c7f101a
brianrgeving <brianrgeving@gmail.com>
2020-07-19 16:58:39 -0700

    Submission for Module2

---
+++ ON/BrianGeving/Module2/data-structure/dictionary.py
@@ -0,0 +1,15 @@
+#accessing
+dict = {'Name': 'abc', 'Age': 7}
+print("Name : ",dict['Name'], "\n", "Age : ", dict['Age'])
+
+#updating
+dict['Age'] = 20
+print("Updated age : ", dict['Age'])
+
+#adding
+dict['Phone_no'] = 123456789
+print("After adding the new pair : ", dict)
+
+#deleting
+del dict['Phone_no']
+print("After deleting Phone_no : ", dict)
---
+++ ON/BrianGeving/Module2/functions-and-files/myFunc.py
@@ -0,0 +1,7 @@
+def printme( str ):
+    print (str)
+    return
+
+#call printme function
+printme("First call to user defined function")
+printme("Second call to the same function")
