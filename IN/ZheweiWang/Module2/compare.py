06c0a902ec8b71bb56e3431715c4e10e2af7ecdf
Zwei <wangzhewei17@hotmail.com>
2020-07-21 00:01:03 -0700

    Submission for Module 2

---
+++ IN/ZheweiWang/Module2/compare.py
@@ -0,0 +1,7 @@
+first=['cats','dogs',55]
+second=['dogs',55,'cats']
+print(first==second)
+
+first_dict={'name':'aaa', 'spacies':'human','age':20}
+second_dict={'spacies':'human','name':'aaa','age':20}
+print(first_dict==second_dict)
