ba8a9a7c5cb850a3f48dadee25caf27ac01f028f
wetwc198892 <103273791@qq.com>
2020-07-20 19:49:33 -0700

    Module2

---
+++ IN/MinQiu/Module2/List_remove.py
@@ -0,0 +1,7 @@
+motorcycles = ['honda','yamaha','suzuki','ducati']
+motorcycles.remove('ducati')
+print(motorcycles)
+print(popped_motorcycle)
+first_owned = motorcycles.pop(0)
+print("The first motorcycle I owned was a" , first_owned)
+
