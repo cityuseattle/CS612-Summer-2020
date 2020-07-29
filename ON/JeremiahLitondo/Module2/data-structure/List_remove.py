5366bd891abf0df51f6238f1561753c7226b7626
jeremiah <jerry.litondo@gmail.com>
2020-07-28 22:38:48 -0700

    Submission for Module2

---
+++ ON/JeremiahLitondo/Module2/data-structure/List_remove.py
@@ -0,0 +1,15 @@
+#del
+motorcycles = ['honda', 'yamaha', 'suzuki']
+#del motorcycles[1]
+popped_motorcycle = motorcycles.pop()
+print(motorcycles)
+
+first_round = motorcycles.pop(0)
+
+print ("The first motorcycle I owned was a ", first_round)
+
+#remove
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+motorcycles.remove('ducati')
+
+print (motorcycles)
