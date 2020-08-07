2a2e076cfd440d6a49201e540afb9a21ad480b04
Xiaoman <xiaoman.zhang2016@gmail.com>
2020-08-06 20:32:33 -0700

    Submission for Module 5

--- IN/XiaomanZhang/Module5/test_1.py
+++
@@ -1,8 +0,0 @@
-try:
-    fh = open("testfile","w")
-    fh.write("this is my test file for exception handling")
-except I0Error:
-    print("error: can\'t find file or read data")
-else:
-    print("written content in the file successfully")
-    fh.close()
--- IN/XiaomanZhang/Module5/test_2.py
+++
@@ -1,8 +0,0 @@
-try:
-    fh = open("testfile","r")
-    fh.write("this is my test file for exception handling")
-except IOError:
-    print("error: can\'t find file or read data")
-else:
-    print("written content in the file successfully")
-    fh.close()
