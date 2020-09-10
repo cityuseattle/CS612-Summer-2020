b99f71837b0fd016fa0dfa7519d2168f55dbb392
marioepugh <marioepugh@gmail.com>
2020-09-10 12:47:54 -0700

    Submission for Module 9

---
+++ IN/MarioPugh/Module9/thisList.py
@@ -0,0 +1,17 @@
+thislist = ["apple", "banana", "cherry"]
+print[thislist]
+
+
+# How to print the first item only
+#thislist = ["apple", "banana", "cherry"]
+#print(thislist[1])
+
+#thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+#print(thislist[2:5])
+
+#thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+#print(thislist[:4])
+
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
